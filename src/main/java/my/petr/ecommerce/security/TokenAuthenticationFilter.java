fae3b1be8f592d1dcc60b898afba781f6ced07a8
Petr Zimin <petrzl@bk.ru>
2019-11-19 23:47:09 +0300

    work with authenticating

--- src/main/java/my/petr/ecommerce/security/TokenAuthenticationFilter.java
+++ src/main/java/my/petr/ecommerce/security/TokenAuthenticationFilter.java
@@ -1,13 +1,7 @@
 package my.petr.ecommerce.security;
 
-
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,11 +9,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Slf4j
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+  private final Log logger = LogFactory.getLog(this.getClass());
 
   @Value("${jwt.header}")
   private String AUTH_HEADER;
+
+//    @Value("${jwt.cookie}")
+//    private String AUTH_COOKIE;
 
   @Autowired
   TokenHelper tokenHelper;
@@ -27,39 +32,62 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
   @Autowired
   UserDetailsService userDetailServiceImpl;
 
-  private String getToken( HttpServletRequest request ) {
+  private String getToken(HttpServletRequest request) {
+
+//        Cookie authCookie = getCookieValueByName( request, AUTH_COOKIE );
+//        if ( authCookie != null ) {
+//            return authCookie.getValue();
+//        }
 
     String authHeader = request.getHeader(AUTH_HEADER);
-    if ( authHeader != null && authHeader.startsWith("Bearer ")) {
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
       return authHeader.substring(7);
     }
 
     return null;
   }
 
-  @Override
-  public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+//    protected Cookie getCookieValueByName(HttpServletRequest request, String name) {
+//        if (request.getCookies() == null) {
+//            return null;
+//        }
+//        for (int i = 0; i < request.getCookies().length; i++) {
+//            if (request.getCookies()[i].getName().equals(name)) {
+//                return request.getCookies()[i];
+//            }
+//        }
+//        return null;
+//    }
 
-    String authToken = getToken( request );
+  @Override
+  public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain chain) throws IOException, ServletException {
+
+    String error = "";
+    String authToken = getToken(request);
     if (authToken != null) {
       // get username from token
-      String username = tokenHelper.getUsernameFromToken( authToken );
-      if ( username != null ) {
+      String username = tokenHelper.getUsernameFromToken(authToken);
+      if (username != null) {
         // get user
-        UserDetails userDetails = userDetailServiceImpl.loadUserByUsername( username );
+        UserDetails userDetails = userDetailServiceImpl.loadUserByUsername(username);
         // create authentication
-        TokenBasedAuthentication authentication = new TokenBasedAuthentication( userDetails );
-        authentication.setToken( authToken );
-        SecurityContextHolder.getContext().setAuthentication( authentication );
+        TokenBasedAuthentication authentication = new TokenBasedAuthentication(userDetails);
+        authentication.setToken(authToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
         chain.doFilter(request, response);
       } else {
-        System.out.println("Username from token can't be found in DB.");
+        error = "Username from token can't be found in DB.";
       }
     } else {
-      System.out.println("No Bearer token provided.");
-      SecurityContextHolder.getContext().setAuthentication( new AnonAuthentication() );
+      error = "No Bearer token provided.";
     }
-
+    if (!error.equals("")) {
+      response.sendError(HttpServletResponse.SC_FORBIDDEN, "Authentication failed");
+      System.out.println("ERROR: " + error);
+      SecurityContextHolder.getContext()
+          .setAuthentication(new AnonAuthentication());//prevent show login form...
+    }
   }
 
 }
